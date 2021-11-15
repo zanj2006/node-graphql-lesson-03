8b153bddad303b3f51a63e9127bbb12781405ef5
Jiří Žán <jirka@jizan.cz>
2021-11-15 18:27:23 +0100

    Initial commit

---
+++ src/schema.js
@@ -0,0 +1,26 @@
+const { gql } = require('apollo-server')
+
+const typeDefs = gql`
+
+  type Student {
+    id: ID!
+    email: String!
+    fullName: String!
+    dept: String
+    enrolled: Boolean
+  }
+
+  type Query {
+    enrollment: [Student!]
+    students: [Student!]!
+    student(id: ID!): Student
+  }
+
+  type Mutation {
+    registerStudent(email: String!, fullName: String!, dept: String): Student!
+    enroll(id: ID!): Student
+  }
+`
+module.exports = {
+  typeDefs,
+}
